a3e06df662890d20466c78fa1d6589dcd517ef5c
khadija garba <naura1835@gmail.com>
2022-06-28 15:31:03 +0100

    delete commented code

--- src/pages/register/register.styles.jsx
+++ src/pages/register/register.styles.jsx
@@ -22,34 +22,6 @@ export const Wrapper = styled(Container)`
       max-width: 500px;
     }
   }
-  /* margin: 5vh 8vw 0;
-  width: 80vw;
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-  grid-template-rows: 80vh;
-  column-gap: 14px;
-
-  
-
-  @media (max-width: 880px) {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-self: flex-start;
-
-    & > svg {
-      width: 50%;
-      align-self: flex-end;
-    }
-  }
-
-  @media (max-width: 636px) {
-    flex-direction: column-reverse;
-
-    & > svg {
-      display: none;
-    }
-  } */
 `;
 
 export const Title = styled(H3)``;
@@ -62,16 +34,6 @@ export const SignUpWrapper = styled.div`
     align-self: center;
     flex: 1;
   }
-  /* align-self: start;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-self: flex-start;
-  width: 80%;
-
-  @media (max-width: 636px) {
-    width: 100%;
-  } */
 `;
 export const OptionGroup = styled.div`
   display: flex;
